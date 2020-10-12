ed0c380ff1a5dd9d413ab061d2a7839740a72e63
Nithya Kalyani <66583899+Nithya-kalyani@users.noreply.github.com>
2020-10-12 11:09:22 +0530

    Add files via upload

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8900;
+const port = process.env.PORT || 8900;
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const mongoUrl = "mongodb+srv://Nithya:mongo@123@cluster0.o3fi9.mongodb.net/edurekainternship?retryWrites=true&w=majority";
